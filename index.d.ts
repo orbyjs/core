5a9fffb1bc7f2f8cde8ca0c296d69487d62bc9ee
Matias Trujillo <mtrujillo@outlook.cl>
2018-12-16 17:14:18 -0300

    add api hooks

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,13 @@
 import {VDom} from "./src/vdom";
+
+interface Options{
+    delay:Number;
+}
+
 declare module "@orby/core"{
-    export function h(tag:String,props?:Object,...children):VDom;
-    export function render(next:VDom,parent:HTMLElement,child:?HTMLElement,context:?Object,isSvg:Boolean):HTMLElement;
-    export function isVDom(any):Boolean;
+    export function h(tag:string,props?:object,...children):VDom;
+    export function render(next:VDom,parent:HTMLElement,child:?HTMLElement,context:?object,isSvg:?boolean):HTMLElement;
+    export function useState(initialState:any):[any,Function,Function];
+    export function useEffect(handler:Function):void;
+    export let option:Option;
 }
