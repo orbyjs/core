9be4376bca342a8062766654948d6289b2f0d458
Matias Trujillo <mtrujillo@outlook.cl>
2018-12-27 00:46:36 -0300

    TS support is modified

--- index.d.ts
+++ index.d.ts
@@ -4,10 +4,30 @@ interface Options{
     delay:Number;
 }
 
+/**
+interface HandlerDefault{
+    (target:HTMLElement|SVGElement)
+}
+interface HandlerUpdate{
+    (target:HTMLElement|SVGElement,prev:object,next:object):boolean|undefined
+}
+    
+interface Props{
+    oncreate:?HandlerDefault,
+    oncreated:?HandlerDefault,
+    onupdate:?HandlerUpdate,
+    onupdated:?HandlerDefault,
+    onremove:?HandlerDefault,
+    onremoved:?HandlerDefault,
+    context:?object,
+    scoped:?boolean,
+}
+*/
+
 declare module "@orby/core"{
     export function h(tag:string,props?:object,...children):VDom;
     export function render(next:VDom,parent:HTMLElement,child:?HTMLElement,context:?object,isSvg:?boolean):HTMLElement;
     export function useState(initialState:any):[any,Function,Function];
-    export function useEffect(handler:Function):void;
+    export function useEffect(handler:Function,list:any[]):void;
     export let option:Option;
 }
