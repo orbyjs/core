d0fda4456831d68bd295106aaa0f57d9dc1c5fd1
Matias Trujillo <mtrujillo@outlook.cl>
2018-12-31 12:41:24 -0300

    the class name VDom => Vtag is modified

---
+++ src/vtag.js
@@ -0,0 +1,48 @@
+export class Vtag {
+    /**
+     *
+     * @param {Function|String} tag - Node component or label
+     * @param {Object} props - Properties of the label
+     * @param {Array} children - Children assigned to the node
+     */
+    constructor(tag, props = {}, children = []) {
+        this.tag = tag;
+        this.props = {
+            ...props,
+            children
+        };
+        this.key = this.props.key;
+    }
+    /**
+     * Clone the current node by keeping props and children by default
+     * @param {Function|String} tag -  Node component or label
+     * @param {*} props - Properties of the label
+     * @param {*} children - Children assigned to the node
+     */
+    clone(tag = this.tag, props = this.props, children = this.props.children) {
+        return new Vtag(tag, props, children);
+    }
+}
+/**
+ * Prepare the virtual node
+ * @param {Function|String} tag
+ * @param {Object} props
+ * @param  {...any} children
+ * @return {Vtag}
+ */
+export function h(tag, props, ...children) {
+    return new Vtag(tag || "", props, concat(children));
+}
+/**
+ * Clean existing values in virtual-dom tree
+ * @param {*} children
+ * @param {*} merge
+ */
+export function concat(children, next = []) {
+    let length = children.length;
+    for (let i = 0; i < length; i++) {
+        let value = children[i];
+        Array.isArray(value) ? concat(value, next) : next.push(value);
+    }
+    return next;
+}
