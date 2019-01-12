374944938405b5e66cac14f14f358191465eea16
Matias Trujillo <mtrujillo@outlook.cl>
2019-01-12 02:24:31 -0300

    content related to rendering is ordered

--- src/diff.js
+++ src/diff.js
@@ -1,48 +1,12 @@
 import { Vtag } from "./vtag";
 import { create, remove, append, replace, root, before } from "./dom";
 import { options } from "./options";
-
-/**
- * stores locally only for the moment of execution of the component,
- * the instance of registration of the same
- */
-let CURRENT_COMPONENT;
-/**
- * state marker associated with the component, it returns to zero with each component execution
- */
-let CURRENT_KEY_STATE;
-
-/**
- * constant for the list of components associated with the node
- */
-export let COMPONENTS = "__COMPONENTS__";
-
-/**static_render
- * this variable allows the hydration of components
-export let STATIC_RENDER = "__STATIC_RENDER__";
-*/
-
-/**
- * constant to store the previous vtag
- */
-export let PREVIOUS = "__PREVIOUS__";
-/**
- * constant to mark the deletion of a node
- */
-export let REMOVE = "__REMOVE__";
-/**
- * Constant to mark events within a node
- */
-export let HANDLERS = "__HANDLERS__";
-
-/**
- * Special properties of virtual dom,
- * these are ignored from the updateProperties process,
- * since it is part of the component's life cycle
- */
-
-export let IGNORE = /^(context|children|(on){1}(Create|Update|Remove)(d){0,1}|xmlns|key|ref)$/;
-
+import { COMPONENTS, PREVIOUS, REMOVE, HANDLERS, IGNORE } from "./constants";
+import {
+    Component,
+    clearComponentEffects,
+    getCurrentComponent
+} from "./component";
 /**
  * It allows to print the status of virtual dom on the planned configuration
  * @param {Vtag} next - the next state of the node
@@ -54,13 +18,6 @@ export let IGNORE = /^(context|children|(on){1}(Create|Update|Remove)(d){0,1}|xm
  */
 export function render(next, parent, child) {
     return updateElement(root(parent), child, false, next);
-}
-/**
- * generates a bottleneck in status updates
- * @param {Function} handler
- */
-export function defer(handler) {
-    setTimeout(handler, options.delay);
 }
 /**
  * allows to issue a property of the object
@@ -143,167 +100,6 @@ export function getPrevious(node, create = true) {
 export function getComponents(node) {
     return node && node[COMPONENTS];
 }
-/**
- * obtains the component in execution, for the connection with the hooks
- * @returns {Vtag}
- */
-export function getCurrentComponent() {
-    if (CURRENT_COMPONENT) {
-        return CURRENT_COMPONENT;
-    }
-    throw new Error(
-        "the hooks can only be called from an existing functional component in the diff queue"
-    );
-}
-/**
- * Allows you to add an observer status of changes to the functional component
- * @param {*} initialState - Initial state to register
- */
-export function useState(initialState) {
-    let key = CURRENT_KEY_STATE++,
-        use = getCurrentComponent();
-
-    if (!(key in use.states)) {
-        use.states.push(
-            typeof initialState === "function" ? initialState() : initialState
-        );
-    }
-    return [
-        use.states[key],
-        nextState => {
-            use.states[key] = nextState;
-            if (use.prevent) return;
-            defer(() => {
-                use.render();
-                use.prevent = false;
-            });
-            use.prevent = true;
-        },
-        () => use.states[key]
-    ];
-}
-/**
- * allows to add an observer effect before the changes of the component
- * note the use of `clearComponentEffects`, this function allows to clean the
- * effects associated with the elimination of the component.
- * @param {Function} handler
- * @param {array} args - allows to issue the handler only when one of the properties is different from the previous one
- */
-export function useEffect(handler, args = []) {
-    let setup,
-        use = getCurrentComponent();
-    args = [].concat(args);
-    let [state] = useState(() => {
-        setup = true;
-        return { args };
-    });
-
-    if (!setup) {
-        if (
-            state.args.length &&
-            !state.args.some((arg, index) => args[index] !== arg)
-        ) {
-            use.effects.prevent[use.effects.updated.length] = true;
-        }
-        state.args = args;
-    }
-    use.effects.updated.push(handler);
-}
-/**
- * returns the current context of the component in execution
- * @param {string} [space]
- */
-export function useContext(space) {
-    let context = getCurrentComponent().context;
-    return space ? context[space] : context;
-}
-/**
- * generates a routing point by associating it with an instance stored in the current node
- * @param {Function} tag - function to associate with the component instance
- * @param {boolean} isSvg - define if the component belongs to a svg tree
- * @param {number} deep - depth index of components
- * @param {Array} currentComponents - group of components associated with the node
- */
-export class Component {
-    constructor(tag, isSvg, deep, currentComponents) {
-        this.isCreate = true;
-        this.base;
-        this.parent;
-        this.tag = tag;
-        this.props = {};
-        this.states = [];
-        this.effects = { remove: [], updated: [] };
-        this.context = {};
-        this.prevent = false;
-        /**
-         * allows to render the current node
-         */
-        this.render = () => {
-            //if (this.prevent) return this.base;
-            if (this.base[REMOVE]) return;
-
-            CURRENT_KEY_STATE = 0;
-            CURRENT_COMPONENT = this;
-
-            this.effects.updated = [];
-            this.effects.prevent = {};
-
-            let nextStateRender = tag(this.props, this.context);
-
-            CURRENT_COMPONENT = false;
-
-            this.clearEffects(true);
-
-            this.base = updateElement(
-                this.parent,
-                this.base,
-                false,
-                nextStateRender,
-                this.context,
-                isSvg,
-                this.isCreate,
-                deep + 1,
-                currentComponents
-            );
-
-            this.gatherEffects();
-
-            this.isCreate = false;
-
-            return this.base;
-        };
-    }
-    /**
-     * cleans the effects associated with the component
-     * @param {boolean} withPrevent - being true uses the effects.prevent property to skip execution
-     *                                this option is given in a cleaning without elimination of the node
-     */
-    clearEffects(withPrevent) {
-        let length = this.effects.remove.length;
-        for (let i = 0; i < length; i++) {
-            let remove = this.effects.remove[i];
-            if (remove && (withPrevent ? !this.effects.prevent[i] : true))
-                remove();
-        }
-    }
-    /**
-     * creates a new deletion effects queue, being true within effect.prevent,
-     * the previous handler is retrieved
-     */
-    gatherEffects() {
-        let remove = [],
-            length = this.effects.updated.length;
-
-        for (let i = 0; i < length; i++) {
-            let handler = this.effects.updated[i];
-            remove[i] = this.effects.prevent[i]
-                ? this.effects.remove[i]
-                : handler();
-        }
-        this.effects.remove = remove;
-    }
-}
-
 /**
  * allows to create, change or update
  * @param {HTMLElement} parent - the container of the node
@@ -408,7 +204,7 @@ export function updateElement(
             return component.base;
         }
 
-        return component.render();
+        return component.update();
     } else if (next.tag) {
         withUpdate =
             emit(next, "onUpdate", base, prev.props, next.props) !== false;
@@ -615,11 +411,4 @@ function recollectNodeTree(node, ignoreEffects) {
     }
 
     emit(prev, "onRemoved", node);
-}
-
-function clearComponentEffects(components) {
-    let length = components.length;
-    for (let i = 0; i < length; i++) {
-        components[i].clearEffects(false);
-    }
 }
