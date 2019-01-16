86a5c99bc72492e26178b9723dc72626c6fc00d4
Matias Trujillo <mtrujillo@outlook.cl>
2019-01-16 13:31:20 -0300

    createContext is added to the export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { useReducer, useContext, useMemo } from "./customHooks";
 export { useState, useEffect, getCurrentComponent } from "./component";
+export { createContext } from "./context";
 export { options } from "./options";
 export { render } from "./diff";
 export { h } from "./vtag";
