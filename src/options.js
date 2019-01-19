c4b1fe2f74071dff7e2ba9df58a09134068db38f
Matias Trujillo <mtrujillo@outlook.cl>
2019-01-19 18:24:44 -0300

    add comments for options properties

--- src/options.js
+++ src/options.js
@@ -1,3 +1,7 @@
+/**
+ * @property {Object} document - allows to replace the document to be used by default within orby
+ * @property {number} delay - controls the microtask waiting time
+ */
 export let options = {
     delay: 0
 };
